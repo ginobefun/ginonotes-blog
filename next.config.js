35ef9ee1cd532fbd3d55865673a73f002fa7aaf2
Gino Zhang <ginobefun@163.com>
2026-08-31 18:41:36 +0800

    fix：修改图片域名白名单

--- next.config.js
+++ next.config.js
@@ -11,6 +11,10 @@ const nextConfig = {
       },
       {
         protocol: 'https',
+        hostname: 'media.bestblogs.dev',
+      },
+      {
+        protocol: 'https',
         hostname: 'images.unsplash.com',
       },
       {
